6c6fe799bd7d4242db5ddd11af7d8907554823f2
cinateevelina <cinateevelina@gmail.com>
2026-09-23 19:52:56 +0200

    ændringer

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 // Skriv "use strict" herunder
 
+"use strict";
+
 function beregnSum(number1, number2) {
+    let sum = number1 + number2;
+     console.log (`Sum er ${sum}`); 
     // Funktionen skal beregne summen af de to tal og udskrive resultatet i konsollen, fx: "Summen af 15 og 27 er 42"
 
 }
 
+beregnSum (15,27); 
+
 
 // Skriv en funktion, der hedder beregnDifference, og som tager to parametre: number1 og number2
+
+function beregnDifference(number1, number2){
+    let dif = number1 - number2;
+    console.log (`dif er ${dif}`);
+}
+
+beregnDifference (27,15);
+
+
 // Funktionen skal beregne forskellen mellem de to tal og udskrive resultatet i konsollen, fx: "Forskellen mellem 27 og 15 er 12"
 
 
